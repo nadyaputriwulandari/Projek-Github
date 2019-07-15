06a9875b31b7c7619fdf2f8c74bf26891a970136
deanira <deanira18ti@mahasiswa.pcr.ac.id>
2019-07-15 17:42:51 +0700

    jogja.php
    wisata jogja

---
+++ jogja.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wisata</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- menghubungkan dengan file jquery -->
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+<!--
+Author : diki alfarabi hadi
+Site : www.malasngoding.com
+-->
+<div class="content">
+	<header>
+		<h1 class="judul">INFORMASI WISATA INDONESIA</h1>
+
+	</header>
+
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+				<ul class="isi-dropdown">
+					<li><a href="bali.php">Bali</a></li>
+					<li><a href="lombok.php">Lombok</a></li>
+					<li><a href="sumbar.php">Sumbar </a></li>
+					<li><a href="jogja.php">Yogyakarta</a></li>
+				</ul>
+			</li>
+			<li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+			<li><a href="tentang.php?page=tutorial">Tentang Kami</a></li>
+			<li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+		</ul>
+	</div>
+
+	<div class="badan">
+		<p><h2><center>Informasi Wisata Yogyakarta</center></h2></p>
+		<p>1. Kaliadem</p>
+		 <style>
+
+    img {
+     width: 430px;
+     height: 200px;
+     border: 5px solid #575D63;
+     padding: 10px;
+     margin-left:auto;
+     margin-right:auto;
+     display:block;
+
+   }
+    p{
+            text-align: justify;
+</style>
+    <img src="kaliadem.jpg">
+
+		<p>  Kalau pernah liat posting-an temanmu berfoto dengan latar Gunung Merapi yang gagah tepat di belakangnya, itu pasti di kawasan Kaliurang. Tepatnya di Bungker Kaliadem. Selain bisa belajar banyak tentang sejarah letusan Gunung Merapi, kamu juga bisa menemukan innerpeace, karena hawanya sejuk, sepi, dan bisa banget buat melepas penat. Kalau kamu sudah bisa foto seperti di atas ini, selamat level petualanganmu baru saja naik satu tingkat!.</p>
+<p>2. Bukit Kali Kuning </p>
+ <img src="kali.jpg">
+<p> Dulunya tempat wisata di Yogyakarta ini pernah ramai wisatawan sebelum letusan Gunung Merapi tahun 2010 silam. Pada awal 2017, Bukit Kali Kuning bersemi kembali menjadi tempat wisata baru yang kece banget buat foto-foto.
+
+Duduk santai sambil menikmati indahnya Gunung Merapi dan hutan sekitarnya yang tenang menjadi kegiatan yang paling menyenangkan. Apalagi ditemani dengan secangkir teh hangat atau kopi panas.</p>
+
+<p>3. Taman Pelangi </p>
+ <img src="pelangi.jpg">
+<p> Masih di utara Yogyakarta, kamu bisa menikmati suasana malam bersama ratusan lampion di sini. Taman Pelangi masih satu kawasan dengan Museum Monumen Jogja Kembali di Jalan Ring Road Utara, Sariharjo, Kabupaten Sleman. Kamu bisa mengunjunginya saat malam, jadi gak cuma ke Alun-alun Kidul atau Malioboro yang sudah ramai pengunjung deh.</p>
+
+<p>4. Kalibiru</p>
+<img src="kalibiru.jpg">
+<p> Lokasinya ada di bagian barat Yogyakarta. Buat kamu yang gak alergi dengan ketinggian, tempat ini jadi destinasi yang seru banget. Kamu bisa berfoto di atas ketinggian dari sebuah panggung dan pohon, dengan latar belakang jurang. Tenang saja, kamu bakal dibekali dengan alat pengaman ketika naik panggung tinggi, kok. Jadi tetap nyaman dan aman saat berfoto ria.
+
+ </p>
+
+<p>5. Hutan Mangrove </p>
+<img src="mangrove.jpg">
+<p> Cocok buat kamu yang mencari rasa baru dari Yogyakarta. Datanglah menjelang senja, suasananya bakal kece banget buat diabadikan dengan kameramu.</p>
+</div>
+</div>
+</body>
+</html>
