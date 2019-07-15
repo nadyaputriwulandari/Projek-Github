dc6b30b7d0310f7a4486acc4455308b60a2311bc
aridwirisky <aridwirisky@gmail.com>
2019-07-15 20:09:34 +0700

    Kontak.php

---
+++ kontak.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wisata</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+
+<div class="content">
+    <header>
+        <h1 class="judul">INFORMASI WISATA INDONESIA</h1>
+
+    </header>
+
+    <div class="menu">
+        <ul>
+            <li><a href="index.php?page=home">HOME</a></li>
+            <li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+                <ul class="isi-dropdown">
+                    <li><a href="bali.php">Bali</a></li>
+                    <li><a href="lombok.php">Lombok</a></li>
+                    <li><a href="sumbar.php">Sumbar </a></li>
+                    <li><a href="jogja.php">Yogyakarta</a></li>
+                </ul>
+            </li>
+            <li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+            <li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+        </ul>
+    </div>
+
+    <div class="badan">
+		 <img src="kontak.png">
+    <style>
+
+   img {
+     width: 937px;
+     height: 500px;
+     border: 1px solid #575D63;
+     padding: 1px;
+   }
+</style>
+    <img src="peta.jpg">
+</body>
+</html>
