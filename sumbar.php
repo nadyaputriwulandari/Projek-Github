813a9c4b55be071dd29b43310eb7814c2db2aaf2
arieee22 <aridwirisky@gmail.com>
2019-07-15 18:48:49 +0700

    Create sumbar.php

---
+++ sumbar.php
@@ -0,0 +1,110 @@
+
+
+
+Lewatkan ke konten
+Menggunakan Email Politeknik Caltex Riau dengan pembaca layar
+
+1 dari 1.433
+puny ari
+
+Nadya Putri Wulandari
+Lampiran
+17.49 (57 menit yang lalu)
+kepada saya
+
+
+Terjemahkan pesan
+Nonaktifkan untuk: Inggris
+
+2 Lampiran
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wisata</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- menghubungkan dengan file jquery -->
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+<!--
+Author : diki alfarabi hadi
+Site : www.malasngoding.com
+-->
+<div class="content">
+	<header>
+		<h1 class="judul">INFORMASI WISATA INDONESIA</h1>
+
+	</header>
+
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+				<ul class="isi-dropdown">
+					<li><a href="bali.php">Bali</a></li>
+					<li><a href="lombok.php">Lombok</a></li>
+					<li><a href="sumbar.php">Sumbar </a></li>
+					<li><a href="jogja.php">Yogyakarta</a></li>
+				</ul>
+			</li>
+			<li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+			<li><a href="tentang.php?page=tutorial">Tentang Kami</a></li>
+			<li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+		</ul>
+	</div>
+
+	<div class="badan">
+		<p><h2><center>Informasi Wisata Sumatra Barat</center></h2></p>
+		<p>1. Danau Singkarak </p>
+		 <style>
+
+   img {
+     width: 430px;
+     height: 200px;
+     border: 5px solid #575D63;
+     padding: 10px;
+     margin-left:auto;
+     margin-right:auto;
+     display:block;
+
+   }
+    p{
+            text-align: justify;
+</style>
+    <img src="singkarak.jpg">
+
+		<p>  Danau Singkarak berjarak 70 km dari Padang, atau sekitar 2 jam waktu tempuh; 20 km dari Solok dan sekitar 36 km dari Bukittinggi. Jika mengambil jalan darat, dari bandara internasional Minangkabau, kita dapat menyewa mobil atau minibus umum dengan rute Padang-Solok dan kemudian mengambil transportasi lain ke danau. Perjalanan menuju Tempat Wisata Menawan Danau Singkarak ini akan membawa kita melewati kawasan Sitinjau Laut yang terkenal dengan tikungan  tajam dan ngarai curam. Pemandangan terbaik dapat dilihat melalui jalan utama Solok-Bukittinggi, dimana pengunjung sering sekali berhenti untuk mengabadikan pemandangan danau yang menakjubkan.
+
+Tour De Singkarak merupakan even tahunan balap sepeda resmi yang diselenggarakan oleh Union Cycliste International sejak tahun 2009.  Melalui even tersebut nama Singkarak, termasuk Danau Singkarak mendunia.</p>
+<p>2. Danau Maninjau </p>
+ <img src="maninjau.jpg">
+<p> Danau Maninjau adalah sebuah danau di kecamatan Tanjung Raya, Kabupaten. Danau Maninjau, yang seluas 99,5 km2, dengan kedalaman hingga 485 m ini merupakan salah satu yang terhening di Indonesia, sehingga cocok untuk tempat menenangkan diri dari suasana hingar-bingar kota, dan menikmati suasana hidup yang hening tenang.
+
+Desa Maninjau terletak di dekat danau dan tempat ini terkenal sebagai tanah kelahiran Buya HAMKA. Seorang novelis  terkenal di Indonesia yang menulis buku fenomenal berjudul “Tenggelamnya Kapal Van der Wijk”. Tokoh lain yang lahir di sini adalah Rangkayo Rasuna Said, yang namanya telah diabadikan sebagai salah satu jalan protokol di Ibu Kota DKI Jakarta, dan diakui sebagai salah satu Pahlawan Nasional Indonesia.
+
+Danau yang terletak sekitar 35 km dari Kota Bukkittinggi ini berkaitan erat dengan kisah Bujang Sembilan. Danau ini dapat dicapai dengan melintasi jalan berkelok yang terkenal dengan sebutan “kelok ampek-ampe” (empat puluh empat kelokan). Aktivitas yang dapat dilakukan di tempat wisata inih, antara lain: menjelajahi daerah yang indah dengan mengayuh sepeda gunung, atau sepeda motor, atau melakukan petualangan yang santai: jelajahi danau dengan kano.</p>
+
+<p>3. Danau Cubadak </p>
+ <img src="cubadak.jpg">
+<p> Pulau Cubadak terletak di Kabupaten Pesisir Selatan, Sumatera Barat, 40 kilometer di sebelah selatan Kota Padang , tepatnya di Kawasan Wisata Mandeh (Mande Resort), dengan luas 705 Ha dan dikenal dengan nama Pincuran Talu. Dapat dicapai dengan kapal dari Pantai Kota Painan Carocok. Pulau Cubadak terdiri dari hutan di mana berbagai jenis satwa hidup, dan hanya didiami beberapa keluarga nelayan dan dikelilingi batu karang. Pulau Cubadak memiliki panorama alam yang sangat indah dengan pasirnya yang putih bersih, dan di sekitarnya terdapat beberapa teluk, batu dan tanjung. </p>
+
+<p>4. Pulau Sikuai</p>
+<img src="sikuai.jpg">
+<p> Pulau Sikuai merupakan sebuah obyek wisata yang terletak di Kepulauan Mentawai, Sumatera Barat dengan luas wilayah sekitar 40 hektar. Pulau Sikuai memiliki pemandangan alam yang masih alami, perpaduan antara keindahan laut dengan segala isinnya, pulau dan hutan yang masih terjaga kelestariannya. Laut di Pulau Sikuai memiliki ombak yang cukup tenang dan memiliki air yang jernih sehingga sangat cocok digunakan untuk aktivitas pantai seperti snorkeling untuk menyaksikan keindahan bawah laut. Berbagai biota laut seperti terumbu karang dan hewan serta aneka binatang yang hidup di laut seperti ikan-ikan hias yang bermacam-macam jenisnya bisa kita temui saat menjelajahi bawah laut Pantai Sikuai.
+
+Jika ingin bersepeda, dipulau ini juga menyediakan menyewaan sepeda yang sudah disiapkan oleh penduduk setempat. Selain berjalan-jalan di sepanjang pantai, kita juga bisa berjalan-jalan mengelilingi pulau. Terkadang akan kita jumpai biawak dan monyet saat kita berjalan menikmati keindahannya. Sore hari merupakan saat-saat yang paling di tunggu oleh wisatawan yang ada di Pulau Sikuai, sebab pada sore hari saatnya menyaksikan sunset. </p>
+
+<p>5. Pantai Carocok </p>
+<img src="carocok.jpg">
+<p> Pantai carocok ini Terletak disebelah barat kota Painan, berjarak kira-kira 2 km dari Pasar Painan. Pantai ini Sangat Terkenal di Sumatera Barat Maupun Indonesia, menurut sebagian orang, Pantai Carocok ini tidak kalah dari Pantai Semenanjung Kra di Malaysia atau Pantai Sanur di Bali. Waktu yang paling indah di pantai Carocok Painan adalah disaat matahari akan tengelam, Panorama Jingga Sunset yang memantul diatas Permukaan Samudera Indonesia Sangat Bagus disaksikan disini.
+
+Dalam kawasan Objek Wisata Pantai Carocok Painan ini Juga terdapat sebuah Pulau Batu Karang yang tersambung dengan ujung Bukit Langkisau.Yaitu Pulau Batu Kareta. Dahulu Pulau Batu Kareta dapat dicapai hanya Pada saat air laut Surut, akan tetapi sejak dibangunnya Jembatan oleh Pihak Pemerintah Kabupaten Pesisir Selatan Pulau Batu Kareta ini dapat dicapai kapan saja karna sudah terintegrasi dengan jembatan.</p>
+</div>
+</div>
+</body>
+</html>
+
+sumbar.php
+Menampilkan paket.php.
