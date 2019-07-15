45a3493bfcde2e9c80e78d66dac94315267d3970
deanira <deanira18ti@mahasiswa.pcr.ac.id>
2019-07-15 17:45:39 +0700

    lombok.php
    wisata lombok

---
+++ lombok.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wisata</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- menghubungkan dengan file jquery -->
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+<!--
+Author : diki alfarabi hadi
+Site : www.malasngoding.com
+-->
+<div class="content">
+	<header>
+		<h1 class="judul">INFORMASI WISATA INDONESIA</h1>
+
+	</header>
+
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+				<ul class="isi-dropdown">
+					<li><a href="bali.php">Bali</a></li>
+					<li><a href="lombok.php">Lombok</a></li>
+					<li><a href="sumbar.php">Sumbar </a></li>
+					<li><a href="jogja.php">Yogyakarta</a></li>
+				</ul>
+			</li>
+			<li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+			<li><a href="tentang.php?page=tutorial">Tentang Kami</a></li>
+			<li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+		</ul>
+	</div>
+
+	<div class="badan">
+		<p><h2><center>Informasi Wisata Lombok</center></h2></p>
+		<p>1. Pantai Seger Kuta </p>
+		 <style>
+
+   img {
+     width: 430px;
+     height: 200px;
+     border: 5px solid #575D63;
+     padding: 10px;
+     margin-left:auto;
+     margin-right:auto;
+     display:block;
+
+   }
+    p{
+            text-align: justify;
+</style>
+    <img src="pantaiseger.jpg">
+
+		<p> Tak hanya Bali, Lombok juga punya Pantai Kuta, lho. Pantai ini merupakan bagian dari kawasan wisata Mandalika yang berada di Lombok Tengah.
+
+Selain menikmati pantai yang ciamik, kamu juga bisa mendaki bukit di sekitar Pantai Seger di kawasan ini. Pemandangan dari atas bukit adalah hamparan pasir putih Pantai Kuta.
+
+Pantai Seger bisa diakses dari Bandara Internasional Lombok dengan kendaraan bermotor. Jaraknya sekitar 21 kilometer. </p>
+<p>2. Taman Wisata Pusuk Sembalu </p>
+ <img src="pusuk.jpg">
+<p> Taman Wisata Pusuk Sembalun berlokasi di kawasan lereng Gunung Rinjani, Desa Sembalun Bumbung, Kabupaten Lombok Timur. Spot wisata ini menyuguhkan semacam gardu pandang dengan pemandangan unik di ketinggian sekitar 1.250 meter di atas permukaan laut.
+
+Sejauh mata memandang, kamu akan dimanjakan dengan hijaunya hutan dan pegunungan. Selain menyegarkan, tempat ini juga Instagramable banget, lho.</p>
+
+<p>3. Pantai Tanjung Aan </p>
+ <img src="aan.jpg">
+<p> Pantai Tanjung Aan berada di Lombok Tengah bagian selatan. Pantai ini sangat terjangkau, karena hanya berjarak sekitar 15 menit dari pusat Kota Mataram
+
+Pantai Tanjung Aan menawarkan panorama yang sangat memesona. Pasirnya putih dan halus, airnya pun jernih kebiruan. Gak heran kalau pantai satu ini sanggup menyegarkan mata dan pikiranmu. </p>
+
+<p>4. Rumah Pohon Gangga </p>
+<img src="pohon.jpg">
+<p> Rumah Pohon Gangga memiliki konsep serupa dengan Taman Wisata Pusuk Sembalun, yakni gardu pandang yang berada di ketinggian. Rumah Pohon Gangga berada di Desa Gangga Mumas, Lombok Utara.
+
+Di tempat ini kamu juga bisa memanjakan matamu dengan pemandangan serba hijau. Lokasinya yang dekat dengan air terjun juga jadi satu hal yang menambah keelokan Rumah Pohon Gangga. </p>
+
+<p>5. Bukit Selong </p>
+<img src="bukit.jpg">
+<p> Bukit Selong atau Bukit Beleq berlokasi di Desa Sembalun Lawang, Kecamatan Sembalun, Kabupaten Lombok Timur. Dari atas Bukit Selong, kamu akan dimanjakan dengan pemandangan yang sangat yakni bukit-bukit yang indah, serta hamparan sawah yang berwarna-warni </p>
+</div>
+</div>
+</body>
+</html>
