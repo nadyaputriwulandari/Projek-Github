60ed5adfb42bb5e17e7e6771c1113f1e780ef2a1
arieee22 <aridwirisky@gmail.com>
2019-07-15 18:26:23 +0700

    paket.php
    paket tour

---
+++ paket.php
@@ -0,0 +1,78 @@
+
+
+
+Lewatkan ke konten
+Menggunakan Email Politeknik Caltex Riau dengan pembaca layar
+
+1 dari 1.433
+puny ari
+
+Nadya Putri Wulandari
+Lampiran
+17.49 (31 menit yang lalu)
+kepada saya
+
+
+Terjemahkan pesan
+Nonaktifkan untuk: Inggris
+
+2 Lampiran
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wisata</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- menghubungkan dengan file jquery -->
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+<!--
+Author : diki alfarabi hadi
+Site : www.malasngoding.com
+-->
+<div class="content">
+	<header>
+		<h1 class="judul">INFORMASI WISATA INDONESIA</h1>
+
+	</header>
+
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+				<ul class="isi-dropdown">
+					<li><a href="bali.php">Bali</a></li>
+					<li><a href="lombok.php">Lombok</a></li>
+					<li><a href="sumbar.php">Sumbar </a></li>
+					<li><a href="jogja.php">Yogyakarta</a></li>
+				</ul>
+			</li>
+			<li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+			<li><a href="tentang.php?page=tutorial">Tentang Kami</a></li>
+			<li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+		</ul>
+	</div>
+
+	<div class="badan">
+		<p><h2><center>PAKET TOUR</center></h2></p>
+			 <style>
+
+   img {
+     width: 900px;
+     height: 500px;
+     border: 1px solid #575D63;
+     padding: 20px;
+   }
+</style>
+		<img src="paketliburan.jpg">
+		<img src="raja.jpg">
+		<img src="sumbar.jpg">
+		<img src="jogja.jpg">
+</div>
+</div>
+</body>
+</html>
+paket.php
+Menampilkan paket.php.
