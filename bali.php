44745217ce1417554187f10279a502171d9ae6c2
deanira <deanira18ti@mahasiswa.pcr.ac.id>
2019-07-15 17:32:41 +0700

    create bali.php

---
+++ bali.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wisata</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- menghubungkan dengan file jquery -->
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+<!--
+Author : diki alfarabi hadi
+Site : www.malasngoding.com
+-->
+<div class="content">
+	<header>
+		<h1 class="judul">INFORMASI WISATA INDONESIA</h1>
+
+	</header>
+
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+				<ul class="isi-dropdown">
+					<li><a href="bali.php">Bali</a></li>
+					<li><a href="lombok.php">Lombok</a></li>
+					<li><a href="sumbar.php">Sumbar </a></li>
+					<li><a href="jogja.php">Yogyakarta</a></li>
+				</ul>
+			</li>
+			<li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+			<li><a href="tentang.php?page=tutorial">Tentang Kami</a></li>
+			<li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+		</ul>
+	</div>
+
+
+	<div class="badan">
+		<p><h2><center> Informasi Wisata Bali</center> </h2></p>
+		<p>1. Pantai Kuta </p>
+		 <style>
+
+   img {
+     width: 430px;
+     height: 200px;
+     border: 5px solid #575D63;
+     padding: 10px;
+     margin-left:auto;
+     margin-right:auto;
+     display:block;
+
+   }
+    p{
+            text-align: justify;
+
+</style>
+    <img src="pantai.jpg">
+
+		<p> Pantai Kuta adalah tempat wisata di Bali yang paling terkenal dan paling banyak dikunjungi wisatawan karena lokasinya yang dekat dengan bandara, pantainya yang indah, biaya yang murah, dan ombaknya yang cocok untuk peselancar pemula. Pantai Kuta juga terkenal dengan panorama matahari tenggelamnya yang sangat indah.
+
+Sebuah fakta unik dari Pantai Kuta adalah sebelum Pantai Kuta menjadi sebuah tempat wisata di Bali yang wajib dikunjungi seperti sekarang ini, Pantai Kuta merupakan sebuah pelabuhan besar, yang merupakan pusat perdagangan di Bali pada masanya.
+
+Dengan pasir putih dan laut birunya, dilengkapi dengan fasilitas pendukung yang sangat lengkap, Pantai Kuta adalah primadona wisata Bali yang harus Anda kunjungi saat berlibur. </p>
+<p>2. Pura Tanah Lot </p>
+ <img src="pura.jpg">
+<p> Pura Tanah Lot adalah salah satu tempat wisata di Bali yang terkenal dengan keindahannya, terutama pada saat matahari terbenam. Pura Tanah Lot yang terdiri dari 2 buah pura merupakan pura tempat memuja dewa laut.
+
+Keunikan utama dari Pura Tanah Lot adalah pura ini berada di atas sebuah batu karang besar di pinggir laut. Pada saat air laut pasang, anda tidak dapat mendekat ke Pura Tanah Lot karena di sekitar batu karang penyangga Pura Tanah Lot akan digenangi air laut. Pada saat air surut anda dapat melihat beberapa ular laut jinak yang menurut penduduk setempat merupakan penjaga Pura Tanah Lot. Selain itu, di lokasi wisata Bali ini anda juga dapat memegang ular suci yang jinak dan tidak berbahaya.</p>
+
+<p>3. Pura Uluwatu </p>
+ <img src="uluwatu.jpg">
+<p> Pura Uluwatu adalah salah satu tempat wisata di Bali yang berada di atas sebuah tebing yang menjorok ke laut. Pura Uluwatu tidak hanya menawarkan suasana religius khas Bali, namun juga menawarkan keindahan panorama, terutama keindahan pemandangan matahari tenggelamnya yang sudah sangat terkenal. Di tempat wisata di Bali yang satu ini, anda akan berjumpa dengan sejumlah kera yang dipercaya berfungsi menjaga kesucian Pura Uluwatu. Untuk memasuki area Pura Uluwatu, anda harus menggunakan sarung dan selendang yang merupakan simbol hormat kepada kesucian Pura Uluwatu. </p>
+
+<p>4. Pantai Jimbaran </p>
+<img src="jimbaran.jpg">
+<p> Pantai Jimbaran adalah salah satu tempat wisata di Bali yang paling terkenal. Pada saat anda datang ke Pantai Jimbaran, yang pertama kali akan anda lihat adalah deretan meja dan kursi makan di atas pasir putih yang indah. Ya, Pantai Jimbaran terkenal dengan kuliner Bali pinggir pantainya, terutama hidangan lautnya.
+
+Tempat wisata di Bali ini sangatlah cocok untuk anda yang ingin berwisata ke pantai sekaligus menikmati wisata kuliner khas Bali. Tidak perlu kuatir menyantap makanan di Pantai Jimbaran karena ombak di Pantai Jimbaran sangatlah tenang, sehingga tidak membahayakan anda yang sedang makan berbagai kuliner khas Bali lezat di pinggir pantai. </p>
+
+<p>5. Danau Batur Kintamani </p>
+<img src="danau.jpg">
+<p> Danau Batur Kintamani merupakan salah satu pesona alam tiada duanya yang dimiliki Bali. Terletak di gunung tertinggi ke 2 di Bali, Danau Batur Kintamani mempunyai hawa yang sejuk dan pemandangan yang sangat mempesona.
+
+Danau Batur Kintamani adalah danau terbesar di Bali yang banyak dikunjungi wisatawan karena menawarkan pemandangan yang tiada duanya di Bali. </p>
+</div>
+</div>
+</body>
+</html>
