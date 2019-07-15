8408e6256df687ed9ec83e696b3cd38015edc2ed
Nadya <nadya18ti@mahasiswa.pcr.ac.id>
2019-07-15 20:27:00 +0700

    galeri.php
    galeri

---
+++ galeri.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wisata</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+
+<div class="content">
+	<header>
+		<h1 class="judul">INFORMASI WISATA INDONESIA</h1>
+		
+	</header>
+
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li class="dropdown"><a href="index.php?page=tentang">DESTINASI</a>
+				<ul class="isi-dropdown"> 
+					<li><a href="bali.php">Bali</a></li>
+					<li><a href="lombok.php">Lombok</a></li>
+					<li><a href="sumbar.php">Sumbar </a></li>
+					<li><a href="jogja.php">Yogyakarta</a></li>
+				</ul>
+			</li>
+			<li><a href="paket.php?page=tutorial">Paket Liburan</a></li>
+			<li><a href="Kontak.php?page=tutorial">Kontak</a></li>
+			<li><a href="galeri.php?page=tutorial">Gallery</a></li>
+		</ul>
+	</div>
+ 
+	<div class="badan">
+		<style>
+
+   img {
+     width: 200px;
+     height: 200px;
+     border: 4px solid #575D63;
+     padding: 10px;
+   }
+    p{
+            text-align: justify;}
+</style>
+<br>
+<p><h3><center>Gallery</center></h3>
+<img class="gambar1" src="aan.jpg"><img class="gambar2" src="bukit.jpg">
+ <img class="gambar3" src="carocok.jpg"><img class="gambar4" src="cubadak.jpg">
+ <img class="gambar5" src="danau.jpg"><img class="gambar6" src="jimbaran.jpg">
+ <img class="gambar7" src="kali.jpg"><img class="gambar8" src="kaliadem.jpg">
+ <img class="gambar9" src="kalibiru.jpg"><img class="gambar10" src="mangrove.jpg">
+ <img class="gambar11" src="maninjau.jpg"><img class="gambar12" src="pantai.jpg">
+  <img class="gambar13" src="pantaiseger.jpg"><img class="gambar14" src="pelangi.jpg">
+ <img class="gambar15" src="pohon.jpg"><img class="gambar16" src="pura.jpg">
+ <img class="gambar17" src="pusuk.jpg"><img class="gambar18" src="sikuai.jpg">
+ <img class="gambar19" src="singkarak.jpg"><img class="gambar20" src="uluwatu.jpg">
+ 
+ 
+
+
+
+	</div>
+</div>
+</body>
+</html>
